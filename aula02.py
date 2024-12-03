824f84cec693c8f1d2002bae1e0d529bafed4d52
Sabrina Moreira <nanamoreiraquadros@gmail.com>
2024-12-03 11:25:52 -0300

    Add files via upload

---
+++ aula02.py
@@ -0,0 +1,12 @@
+print("bom dia!")
+print("hoje, iremos aprender python")
+
+nome_usuario = "Sabrina Moreira" # tipo -> str = string
+idade_usuario = 20 # tipo -> int = integer
+altura_usuario = 1.53 # tipo -> float = ponto flutante 
+maior_idade = True # tipo -> bool = boolean
+
+print("seja bem-vinda!" , nome_usuario)
+print("sua idade é" , idade_usuario, "anos")
+print("você tem" , altura_usuario , "metros.")
+print("você é maior de idade?" , maior_idade)
